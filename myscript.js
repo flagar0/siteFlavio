ebbe6e126686afdabc9f4562f9a36bdd1c88329f
flagar0 <flaben10@gmail.com>
2021-04-09 15:12:46 -0300

    Update ColorCycle

--- myscript.js
+++ myscript.js
@@ -74,3 +74,30 @@ function Copiar(){
     }
 }
 
+
+function mudarCor(){
+    fundo = document.getElementById('cycler');
+
+    var antigo =  rgb2hex(fundo.style.backgroundColor)
+    antigo = antigo.replace(/#/g, '');
+    var hexa = parseInt(antigo,16);
+    var valor =  hexa + 64;
+
+    var final = valor.toString(16)
+
+
+    fundo.style.backgroundColor =  "#"+   final
+}
+
+var hexDigits = new Array
+        ("0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f"); 
+
+//Function to convert rgb color to hex format
+function rgb2hex(rgb) {
+ rgb = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
+ return "#" + hex(rgb[1]) + hex(rgb[2]) + hex(rgb[3]);
+}
+function hex(x) {
+    return isNaN(x) ? "00" : hexDigits[(x - x % 16) / 16] + hexDigits[x % 16];
+   }
+
